dad87023326c7db9b7a3c27d023827482eeeda37
кто <65656972+xDanilcusx@users.noreply.github.com>
2024-01-01 16:01:25 +0300

    Fabric Mod Template

---
+++ src/client/java/danilcus/mementomori/MementoMoriClient.java
@@ -0,0 +1,10 @@
+package danilcus.mementomori;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MementoMoriClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
